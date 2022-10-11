d111a14d3d78955d8bbe57b5c9e6a0b3099cc054
jackinater334 <106994419+jackinater334@users.noreply.github.com>
2022-10-11 10:06:16 -0600

    proget 2.3
    making a backound and ground

---
+++ 2.2chalige/Assets/code/player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log
+         bool lives;
+        lives = true;
+        lives = false;
+        int lives = 4;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
