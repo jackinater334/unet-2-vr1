4521e36bcba1142082a523f81b4fa0021677180d
jackinater334 <106994419+jackinater334@users.noreply.github.com>
2022-10-14 10:30:39 -0600

    2.4 chalinge
    i made the player able to  stand on the ground

---
+++ claning 2.4/Assets/code/primitave.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class primitave : MonoBehaviour
+{
+    int hp = 10;
+    Rigidbody2D a = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(hp);
+        Debug.Log(a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
