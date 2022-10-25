4e17185d9136586610405b49a44217d372806aa5
jackinater334 <106994419+jackinater334@users.noreply.github.com>
2022-10-25 10:38:58 -0600

    2.5
    we used interrs

---
+++ 2.5 chalinge/Assets/scrip/my_math.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class my_math : MonoBehaviour
+{
+    int num =1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(num);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        prodet();
+        Debug.Log(num);
+
+    }
+
+    void prodet()
+    {
+        num = num * 2;
+     }
+}
