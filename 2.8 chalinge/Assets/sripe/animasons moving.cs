18d86d764ceb9222176f1f6da7df5d9f002b69ef
jackinater334 <106994419+jackinater334@users.noreply.github.com>
2022-11-10 11:32:08 -0700

    2.8 chalinge

---
+++ 2.8 chalinge/Assets/sripe/animasons moving.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D m_Rigidbody;
+
+    public float jumpForce = 20f;
+    int jumpsLeft;
+
+    public Animator animator;
+
+    public int xspeed = 4;
+
+
+    Vector2 xMove;
+
+    Transform player;
+
+    float xMovement;
+
+    float yMovement;
+
+    void Start()
+    {
+        player = gameObject.transform;
+
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMovement = Input.GetAxis("Horizontal");
+
+        yMovement = Input.GetAxis("Vertical");
+
+        xMove = new Vector2(xMovement, yMovement);
+
+        player.Translate(xMove * Time.deltaTime);
+
+        if (xMovement > 0)
+        {
+            player.localScale = new Vector3(3, 3, 1);
+        }
+        if (xMovement < 0)
+        {
+            player.localScale = new Vector3(-3, 3, 1);
+        }
+        animator.SetFloat("speed", Mathf.Abs(xMovement));
+        if (Input.GetKeyDown(KeyCode.z)||Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.Addforce(transform.up * jumpForce);
+        }
+
+
+    }
+}
