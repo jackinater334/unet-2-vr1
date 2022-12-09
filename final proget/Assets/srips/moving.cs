be209f49d6971f2a661988b7edc034e34485a45c
jackinater334 <106994419+jackinater334@users.noreply.github.com>
2022-12-09 11:20:57 -0700

    player movement

--- final proget/Assets/srips/moving.cs
+++ final proget/Assets/srips/moving.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class animasonsmoving : MonoBehaviour
+public class moving : MonoBehaviour
 {
     // Start is called before the first frame update
-    
 
-  
 
-    
 
-    public int xspeed = 4;
+
+
+
+    public int xspeed = 10;
 
     Vector2 xMove;
 
@@ -21,37 +21,48 @@ public class animasonsmoving : MonoBehaviour
 
     float yMovement;
 
-    public int Thrust = 200;
+    Rigidbody2D rb;
 
+    public int Thrust = 250;
+    
     void Start()
     {
         player = gameObject.transform;
 
-        m_Rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //bacic movement
         xMovement = Input.GetAxis("Horizontal");
 
         yMovement = Input.GetAxis("Vertical");
 
         xMove = new Vector2(xMovement, yMovement);
-
-        player.Translate(xMove * Time.deltaTime);
+        
+        player.Translate(xMove *xspeed * Time.deltaTime);
 
         if (xMovement > 0)
         {
-            player.localScale = new Vector3(3, 3, 1);
+            player.localScale = new Vector3(2, 2, 1);
         }
         if (xMovement < 0)
         {
-            player.localScale = new Vector3(-3, 3, 1);
+            player.localScale = new Vector3(-2, 2, 1);
         }
-        animator.SetFloat("speed", Mathf.Abs(xMovement));
+        
 
+        //jumping code
 
+        if (rb.velocity.y == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Z)||Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rb.AddForce(transform.up * Thrust);
+            }
+        }
 
     }
 }
