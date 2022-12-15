1139810296e50e3fbce2c7a1b4f11164f21847c3
jackinater334 <106994419+jackinater334@users.noreply.github.com>
2022-12-15 11:12:40 -0700

    aaaaaaaaaaa

---
+++ final proget/Assets/srips/game_over.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class game_over : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(newLevel);
+        }
+    }
+}
+
---
+++ final proget/Assets/srips/protol.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class protol : MonoBehaviour
+{
+    public float speed = 2;
+    int dir = 1;
+    public Transform rightCheck;
+    public Transform leftCheck;
+
+
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+        transform.Translate(Vector2.right * speed * dir * Time.fixedDeltaTime);
+        if (Physics2D.Raycast(rightCheck.position, Vector2.down, 2) == false)
+            dir = -1;
+
+        if (Physics2D.Raycast(leftCheck.position, Vector2.down, 2) == false)
+            dir = 1;
+    }
+}
